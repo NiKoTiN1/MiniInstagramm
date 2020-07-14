4eea627a16f7775194935823dc5f2a05b2b503b2
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2020-07-14 16:32:11 +0300

    what

---
+++ MiniInstagram.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MiniInstagram.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MiniInstagram.Web/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniInstagram.Web.Models
+{
+    public class Image
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Path { get; set; }
+        public ApplicationUser User { get; set; }
+        public DateTime UploadDate { get; set; }
+        public Category category { get; set; } = Category.Other;
+    }
+}
