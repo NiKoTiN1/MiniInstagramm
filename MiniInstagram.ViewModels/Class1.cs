edea0d2e59eff94ec712a1e85a028217a4d08ba5
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2020-07-15 14:59:09 +0300

    architecture update

---
+++ MiniInstagram.ViewModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniInstagram.ViewModels
+{
+    public class Class1
+    {
+    }
+}
