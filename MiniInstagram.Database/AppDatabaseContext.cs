ae3a7b2350f0f232bf79bc1de2364e622f7e8616
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2020-07-16 19:23:04 +0300

    DB Init

---
+++ MiniInstagram.Database/AppDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using MiniInstagram.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniInstagram.Database
+{
+    class AppDatabaseContext: DbContext
+    {
+        DbSet<Image> Images { get; set; }
+    }
+}
---
+++ MiniInstagram.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MiniInstagram.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MiniInstagram.Database.AppDatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MiniInstagram.Database.AppDatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
